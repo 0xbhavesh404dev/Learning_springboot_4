035aaae41ab45081792b019e0718f84bae0ad67b
Bhavesh <Kbhavesh2007@gmail.com>
2026-07-15 18:50:41 +0530

    added Student entity, repository, service, and controller with CRUD operations

--- college_campushub/src/main/java/com/bhavesh/college_campushub/Controller/StudentController.java
+++ college_campushub/src/main/java/com/bhavesh/college_campushub/Controller/StudentController.java
@@ -1,4 +1,51 @@
 package com.bhavesh.college_campushub.Controller;
 
+import com.bhavesh.college_campushub.Services.StudentService;
+import com.bhavesh.college_campushub.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    // Create
+    @PostMapping
+    public Student createStudent(@RequestBody Student student){
+        return studentService.saveStudent(student);
+    }
+
+    // Get All
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+    // Get By Id
+    @GetMapping("/{id}")
+    public Optional<Student> getStudentById(@PathVariable String id){
+        return studentService.getStudentById(id);
+    }
+
+    // Update
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable String id,
+                                 @RequestBody Student student){
+        return studentService.updateStudent(id, student);
+    }
+
+    // Delete
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable String id){
+
+        studentService.deleteStudent(id);
+
+        return "Student Deleted Successfully";
+    }
 }
--- college_campushub/src/main/java/com/bhavesh/college_campushub/entity/Student.java
+++ college_campushub/src/main/java/com/bhavesh/college_campushub/entity/Student.java
@@ -1,17 +1,27 @@
 package com.bhavesh.college_campushub.entity;
 
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
+@Document(collection = "student")
 public class Student {
-    private String id;
-    private String name;
-    private String email;
-    private String phone;
-    private String semester;
-    private Department department;
 
+    @Id
+    private String id;
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private String semester;
+
+    @DBRef
+    private Department department;
 }
