8bad5dc65bd7ab26c1571c6028b0c74d2e667b13
Bhavesh <Kbhavesh2007@gmail.com>
2026-07-15 18:12:56 +0530

    added Course entity, repository, service, and controller with CRUD operations

--- college_campushub/src/main/java/com/bhavesh/college_campushub/Controller/CourseController.java
+++ college_campushub/src/main/java/com/bhavesh/college_campushub/Controller/CourseController.java
@@ -1,18 +1,51 @@
 package com.bhavesh.college_campushub.Controller;
 
-
-import com.bhavesh.college_campushub.Repository.DepartmentRepository;
-import com.bhavesh.college_campushub.Services.DepartmentService;
+import com.bhavesh.college_campushub.Services.CourseService;
+import com.bhavesh.college_campushub.entity.Course;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@Configuration
+@RequestMapping("/course")
 public class CourseController {
 
+    @Autowired
+    private CourseService courseService;
 
+    // Create
+    @PostMapping
+    public Course createCourse(@RequestBody Course course){
+        return courseService.saveCourse(course);
+    }
 
+    // Get All
+    @GetMapping
+    public List<Course> getAllCourses(){
+        return courseService.getAllCourses();
+    }
 
+    // Get By Id
+    @GetMapping("/{id}")
+    public Optional<Course> getCourseById(@PathVariable String id){
+        return courseService.getCourseById(id);
+    }
+
+    // Update
+    @PutMapping("/{id}")
+    public Course updateCourse(@PathVariable String id,
+                               @RequestBody Course course){
+        return courseService.updateCourse(id, course);
+    }
+
+    // Delete
+    @DeleteMapping("/{id}")
+    public String deleteCourse(@PathVariable String id){
+
+        courseService.deleteCourse(id);
+
+        return "Course Deleted Successfully";
+    }
 }
--- college_campushub/src/main/java/com/bhavesh/college_campushub/entity/Course.java
+++ college_campushub/src/main/java/com/bhavesh/college_campushub/entity/Course.java
@@ -1,26 +1,25 @@
 package com.bhavesh.college_campushub.entity;
 
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
-public class course{
+@Document(collection = "course")
+public class Course {
+
+    @Id
     private String id;
+
     private String courseName;
+
     private String courseCode;
+
     private int credits;
-    private String professor;
+
+    private Professor professor;
+
     private Department department;
 }
-
-//
-//Department
-//      │
-//      ├──────── Professor
-//      │
-//      ├──────── Student
-//      │
-//      └──────── Course
