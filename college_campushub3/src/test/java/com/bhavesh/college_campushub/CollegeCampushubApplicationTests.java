aaf0b0b53c186d6216c24c559c44978d3a6fcc0f
Bhavesh <Kbhavesh2007@gmail.com>
2026-07-27 01:16:28 +0530

    Add unit test for CollegeCampushub application context

---
+++ college_campushub3/src/test/java/com/bhavesh/college_campushub/CollegeCampushubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavesh.college_campushub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeCampushubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
