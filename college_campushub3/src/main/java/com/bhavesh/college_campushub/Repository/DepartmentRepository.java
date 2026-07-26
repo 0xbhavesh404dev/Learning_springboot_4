b20ef94442dabbf44c1c323bc7aafe54c9a64c7c
Bhavesh Kumar <kbhavesh2007@gmail.com>
2026-07-27 02:23:42 +0530

    Revert "Add DepartmentService and DepartmentRepository for department management"
    This reverts commit bf7affdbe30f866df2f6e454eeeaa3211109c1b1.

--- college_campushub3/src/main/java/com/bhavesh/college_campushub/Repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bhavesh.college_campushub.Repository;
-
-import com.bhavesh.college_campushub.entity.Department;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DepartmentRepository extends MongoRepository<Department, String> {
-
-
-}
